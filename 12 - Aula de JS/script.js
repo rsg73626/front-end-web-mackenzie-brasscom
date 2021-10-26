4f4b5bacfb6aca853fd7ceb927083127c4e520ff
Renan Germano <renangermano@MacBook-Pro-de-Renan-2.local>
2021-10-26 16:08:12 -0300

    Primeira aula de JavaScript.

---
+++ 12 - Aula de JS/script.js
@@ -0,0 +1,5 @@
+document.getElementById("exemplo-8").style.width = "100px";
+document.getElementById("exemplo-8").style.height = "100px";
+document.getElementById("exemplo-8").style.border = "1px solid black";
+document.getElementById("exemplo-8").style.borderRadius = "10px";
+document.getElementById("exemplo-8").style.textAlign = "center";
